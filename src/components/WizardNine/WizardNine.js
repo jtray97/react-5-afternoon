d93e431be72ca8742dca87b168a73477b65eb5bc
unknown <jtray@outlook.es>
2018-06-25 16:36:12 -0600

    done with 9

--- src/components/WizardNine/WizardNine.js
+++ src/components/WizardNine/WizardNine.js
@@ -1,18 +1,21 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class WizardNine extends Component {
+import { connect } from 'react-redux';
+import { updateAddOne, updateAddTwo, updateAddThree } from '../../ducks/reducer';
 
+class WizardNine extends Component {
     render(){
+        const{ updateAddOne, updateAddTwo, updateAddThree} = this.props
         return(
             <div className="parent-div">
                 <div className="vert-align">                    
                     
                     <p>What is your address?</p> <br />
 
-                    <input type="text" placeholder="Line One" onChange={this.props.updateAddLineOne}/>
-                    <input type="text" placeholder="Line Two" onChange={this.props.updateAddLineTwo}/>
-                    <input type="text" placeholder="Line Three" onChange={this.props.updateAddLineThree}/>
+                    <input type="text" placeholder="Line One" onChange={(e)=>{updateAddOne(e.target.value)}}/>
+                    <input type="text" placeholder="Line Two" onChange={(e)=>{updateAddTwo(e.target.value)}}/>
+                    <input type="text" placeholder="Line Three" onChange={(e)=>{updateAddThree(e.target.value)}}/>
                 
                     
                     <Link to="/wTen"><button className="margin-btn"> Next </button></Link>
@@ -22,4 +25,14 @@ class WizardNine extends Component {
     }
 }
 
-export default WizardNine;
+function mapStateToProps (state){
+    const {addressOne, addressTwo, addressThree} = state;
+    return{
+        addressOne,
+        addressTwo,
+        addressThree
+    }
+
+}
+
+export default connect(mapStateToProps, {updateAddOne, updateAddThree, updateAddTwo})( WizardNine);
